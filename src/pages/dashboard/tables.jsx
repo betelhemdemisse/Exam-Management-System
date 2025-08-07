e5b29a2b81fc3f9514af1cb82c537d5cf4b1be99
henok_mule <anamulatu036@gmail.com>
2025-08-07 10:40:01 +0300

    first push

---
+++ src/pages/dashboard/tables.jsx
@@ -0,0 +1,52 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Avatar,
+  Chip,
+  Tooltip,
+  Progress,
+} from "@material-tailwind/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import { authorsTableData } from "@/data";
+
+export function Tables() {
+  return (
+    <div className="mt-12 mb-8 flex flex-col gap-12">
+  
+      <Card>
+      
+        <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+          <table className="w-full min-w-[640px] table-auto">
+            <thead>
+              <tr>
+                {["companies", "members", "budget", "completion", ""].map(
+                  (el) => (
+                    <th
+                      key={el}
+                      className="border-b border-blue-gray-50 py-3 px-5 text-left"
+                    >
+                      <Typography
+                        variant="small"
+                        className="text-[11px] font-bold uppercase text-blue-gray-400"
+                      >
+                        {el}
+                      </Typography>
+                    </th>
+                  )
+                )}
+              </tr>
+            </thead>
+            <tbody>
+             
+      
+            </tbody>
+          </table>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+export default Tables;
